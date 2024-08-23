2eb2457fc9ebf57649d173427cbf68b3bb27fbcd
Moksh Bansal <68209883+moksh07b@users.noreply.github.com>
2024-08-23 20:03:44 +0530

    Create secrets.h

---
+++ secrets.h
@@ -0,0 +1,8 @@
+// Use this file to store all of the private credentials 
+// and connection details
+
+#define SECRET_SSID "Moksh"		// My SSID of WiFi network name
+#define SECRET_PASS "1234567890"	// My WiFi password
+
+#define SECRET_CH_ID 2634904			// My Channel ID
+#define SECRET_WRITE_APIKEY "CYWD9UL02GLITXWS"   // My API Key
